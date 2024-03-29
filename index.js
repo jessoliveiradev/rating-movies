f995e9bab2a03906c9a10a9b13e857d8c6f2d11e
Jessica <jessicaoliveira.dev1@gmail.com>
2024-03-29 00:10:04 -0300

    refactor function

--- index.js
+++ index.js
@@ -25,15 +25,7 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
 });
 
 sequelize.authenticate()
-  .then(() => {
-    console.log('Conexão bem-sucedida com o banco de dados.');
-  })
-  .catch(err => {
-    console.error('Erro ao conectar ao banco de dados:', err);
-  });
+  .then(() => console.log('Conexão bem-sucedida com o banco de dados.'))
+  .catch(err => console.error('Erro ao conectar ao banco de dados:', err));
 
-
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
